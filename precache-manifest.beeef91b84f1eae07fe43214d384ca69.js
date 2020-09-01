bc11d01fc59caa0c2107ce0ef724eec5c56eb465
proprk <notyetfamousprk@gmail.com>
2020-09-01 16:51:57 +0530

    Updates

---
+++ precache-manifest.beeef91b84f1eae07fe43214d384ca69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddf1475e16fceeccf38433d49b313d09",
+    "url": "/chatbook/index.html"
+  },
+  {
+    "revision": "0e3692d311d8c060dfa2",
+    "url": "/chatbook/static/css/main.4bb8270d.chunk.css"
+  },
+  {
+    "revision": "7ab752aff3559bb14502",
+    "url": "/chatbook/static/js/2.e4a12b3d.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/chatbook/static/js/2.e4a12b3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e3692d311d8c060dfa2",
+    "url": "/chatbook/static/js/main.223755d1.chunk.js"
+  },
+  {
+    "revision": "9dbc73ded426730613f0",
+    "url": "/chatbook/static/js/runtime-main.22b5f2d4.js"
+  },
+  {
+    "revision": "adbccee0708ae3b7a71d9652fb353299",
+    "url": "/chatbook/static/media/whatspp-bg.adbccee0.png"
+  }
+]);
